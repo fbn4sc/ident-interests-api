5b6e834232dd7bf81acbe314a83152f16db227d5
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-11 15:25:58 +0000

    Fixes env variable name

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("iDent Challenge API");
+  res.send("Interests API");
 });
 
 app.get("/interests", (req, res) => {
--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: process.env.HOST,
+  host: process.env.DATABASEHOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE
